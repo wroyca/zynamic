e0183b0e2cbce09917363a2553e3db46ff91f791
William Roy <wroy@pm.me>
2022-04-27 18:33:39 -0400

    Downgrade the code to build dinosaurs from fossils
    Signed-off-by: William Roy <wroy@pm.me>

--- reflective_pe.cpp
+++ reflective_pe.cpp
@@ -2,10 +2,10 @@
 //! SPDX-License-Identifier: GPL-3.0-or-later
 //! code based on https://github.com/momo5502/open-iw5/blob/master/src/loader/loader.cpp
 
-#if !__has_include("stdafx.hpp")
-#include <filesystem>
+#include <Windows.h>
+#include <atlstr.h>
+#include <vector>
 #include <fstream>
-#endif // !__has_include("stdafx.hpp")
 
 #pragma bss_seg      (".reflective_pe")
                    char reflective_pe[0x0A000000];
@@ -27,54 +27,54 @@ struct PE
 
 void load(std::vector<char> pe)
 {
-  PE src{}, dst{};
+  PE src, dst;
 
   src.image = reinterpret_cast<HMODULE>(&pe.at(0));
   src.image_dos_header = reinterpret_cast<PIMAGE_DOS_HEADER>(src.image);
   src.image_nt_headers = reinterpret_cast<PIMAGE_NT_HEADERS>(reinterpret_cast<unsigned>(src.image) + src.image_dos_header->e_lfanew);
-  dst.image = GetModuleHandleA(nullptr);
+  dst.image = GetModuleHandleA(NULL);
   dst.image_dos_header = reinterpret_cast<PIMAGE_DOS_HEADER>(dst.image);
   dst.image_nt_headers = reinterpret_cast<PIMAGE_NT_HEADERS>(reinterpret_cast<unsigned>(dst.image) + dst.image_dos_header->e_lfanew);
 
-  auto lpfl_old_protect = 0ul;
-  auto section = IMAGE_FIRST_SECTION(src.image_nt_headers);
+  unsigned long lpfl_old_protect = 0ul;
+  PIMAGE_SECTION_HEADER section = IMAGE_FIRST_SECTION(src.image_nt_headers);
 
-  for (auto i = 0; i < src.image_nt_headers->FileHeader.NumberOfSections; ++i, ++section)
+  for (int i = 0; i < src.image_nt_headers->FileHeader.NumberOfSections; ++i, ++section)
   {
     if (!section || section->SizeOfRawData <= 0)
       continue;
 
-    auto section_virtual_address = reinterpret_cast<void*>(reinterpret_cast<unsigned>(dst.image) + section->VirtualAddress);
-    auto section_raw_data = reinterpret_cast<void*>(reinterpret_cast<unsigned>(src.image) + section->PointerToRawData);
-    auto section_raw_data_size = (std::min)(section->SizeOfRawData, section->Misc.VirtualSize);
+    void *section_virtual_address = reinterpret_cast<void*>(reinterpret_cast<unsigned>(dst.image) + section->VirtualAddress);
+    void *section_raw_data = reinterpret_cast<void*>(reinterpret_cast<unsigned>(src.image) + section->PointerToRawData);
+    unsigned long section_raw_data_size = (std::min)(section->SizeOfRawData, section->Misc.VirtualSize);
 
     VirtualProtect(section_virtual_address, section_raw_data_size, PAGE_EXECUTE_READWRITE, &lpfl_old_protect);
     memmove(section_virtual_address, section_raw_data, section_raw_data_size);
     VirtualProtect(section_virtual_address, section_raw_data_size, lpfl_old_protect, &lpfl_old_protect);
   }
 
-  auto import_directory = &src.image_nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
-  auto import_descriptor = reinterpret_cast<PIMAGE_IMPORT_DESCRIPTOR>(reinterpret_cast<unsigned>(dst.image) + import_directory->VirtualAddress);
+  PIMAGE_DATA_DIRECTORY import_directory = &src.image_nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+  PIMAGE_IMPORT_DESCRIPTOR import_descriptor = reinterpret_cast<PIMAGE_IMPORT_DESCRIPTOR>(reinterpret_cast<unsigned>(dst.image) + import_directory->VirtualAddress);
 
   while (import_descriptor->Name)
   {
-    auto import_descriptor_name = reinterpret_cast<char*>(reinterpret_cast<unsigned>(dst.image) + import_descriptor->Name);
-    auto import_descriptor_first_thunk = reinterpret_cast<unsigned*>(reinterpret_cast<unsigned>(dst.image) + import_descriptor->FirstThunk);
-    auto import_descriptor_original_first_thunk = reinterpret_cast<unsigned*>(reinterpret_cast<unsigned>(dst.image) + import_descriptor->OriginalFirstThunk);
+    char *import_descriptor_name = reinterpret_cast<char*>(reinterpret_cast<unsigned>(dst.image) + import_descriptor->Name);
+    unsigned *import_descriptor_first_thunk = reinterpret_cast<unsigned*>(reinterpret_cast<unsigned>(dst.image) + import_descriptor->FirstThunk);
+    unsigned *import_descriptor_original_first_thunk = reinterpret_cast<unsigned*>(reinterpret_cast<unsigned>(dst.image) + import_descriptor->OriginalFirstThunk);
 
     while (*import_descriptor_original_first_thunk)
     {
-      auto import_descriptor_library = LoadLibraryA(import_descriptor_name);
-      auto import_descriptor_lookup_table = reinterpret_cast<PIMAGE_IMPORT_BY_NAME>(reinterpret_cast<unsigned>(dst.image) + *import_descriptor_original_first_thunk);
-      auto import_descriptor_address_table = static_cast<FARPROC>(nullptr);
+      HMODULE import_descriptor_library = LoadLibraryA(import_descriptor_name);
+      PIMAGE_IMPORT_BY_NAME import_descriptor_lookup_table = reinterpret_cast<PIMAGE_IMPORT_BY_NAME>(reinterpret_cast<unsigned>(dst.image) + *import_descriptor_original_first_thunk);
+      FARPROC import_descriptor_address_table;
 
       if (!import_descriptor_library)
       {
-        MessageBox(nullptr, (std::stringstream() << "The code execution cannot proceed because " << import_descriptor_name << " was not found. Reinstalling the program may fix this problem.").str().c_str(), "System Error", MB_ICONERROR);
-        std::quick_exit(EXIT_FAILURE);
+        MessageBox(NULL, std::string("The code execution cannot proceed because").append(" ").append(import_descriptor_name).append(" ").append("was not found. Reinstalling the program may fix this problem.").c_str(), "System Error", MB_ICONERROR);
+        _exit(EXIT_FAILURE);
       }
 
-      import_descriptor_address_table = IMAGE_SNAP_BY_ORDINAL(*import_descriptor_original_first_thunk) ? GetProcAddress(import_descriptor_library, MAKEINTRESOURCEA(IMAGE_ORDINAL(*import_descriptor_original_first_thunk))) : GetProcAddress(import_descriptor_library, &import_descriptor_lookup_table->Name[0]);
+      import_descriptor_address_table = IMAGE_SNAP_BY_ORDINAL(*import_descriptor_original_first_thunk) ? GetProcAddress(import_descriptor_library, MAKEINTRESOURCEA(IMAGE_ORDINAL(*import_descriptor_original_first_thunk))) : GetProcAddress(import_descriptor_library, reinterpret_cast<LPCSTR>(&import_descriptor_lookup_table->Name[0]));
      *import_descriptor_first_thunk = reinterpret_cast<unsigned>(import_descriptor_address_table);
       import_descriptor_first_thunk++;
       import_descriptor_original_first_thunk++;
@@ -101,11 +101,11 @@ int main(int argc, char* argv[])
   if (argc < 2)
     return 1;
 
-  auto fs = std::filesystem::path(argv[1]);
-  auto fs_name = fs.filename();
-  auto fs_relative = fs.parent_path();
+  CString fs_relative = argv[1];
+  PathRemoveFileSpec(fs_relative.GetBuffer(0));
+  fs_relative.ReleaseBuffer(-1);
 
-  SetCurrentDirectoryA(fs_relative.string().c_str()); // NOTE: Not foolproof. Some PEs try to read files using an incorrect Cwd independently.
+  SetCurrentDirectory(fs_relative); // NOTE: Not foolproof. Some PEs try to read files using an incorrect Cwd independently.
   memset(reflective_pe_thread_local_storage, 0, sizeof reflective_pe_thread_local_storage);
-  ReflectivePE::load(std::vector(std::istreambuf_iterator(std::ifstream(fs_name, std::ios::binary).rdbuf()), std::istreambuf_iterator<char>()));
+  ReflectivePE::load(std::vector<char>(std::istreambuf_iterator<char>(std::ifstream(argv[1], std::ios::binary).rdbuf()), std::istreambuf_iterator<char>()));
 }
