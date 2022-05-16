4d8145a00c20266d9ac03d89559dca56d8f808b5
William Roy <wroy@pm.me>
2022-05-16 18:03:07 -0400

    Catch scope resolution
    Signed-off-by: William Roy <wroy@pm.me>

--- src/zynamic/zynamic.cpp
+++ src/zynamic/zynamic.cpp
@@ -255,9 +255,9 @@ auto load()
       // reserved symbol. So we create a special set of rules to
       // filter the problematic ones.
       //
-      // TODO: Regex?
+      // TODO: Regex, class supports.
       //
-      auto keywords = { L"std::"s, L"atexit"s, L"operator"s, L"__"s, L"~"s, L"@"s, L"?"s };
+      auto keywords = { L"std::"s, L"atexit"s, L"operator"s, L"__"s, L"~"s, L"@"s, L"?"s, L"::"s };
       auto reserved = std::any_of(std::begin(keywords), std::end(keywords), [&](const std::wstring& keyword)
       {
         return std::wstring(name).find(keyword) != std::string::npos;
